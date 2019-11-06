2a2c4adb80a13e304fac0aff9bfbd0859143af26
Diane Wu <dianewudw@gmail.com>
2019-11-05 19:04:33 -0500

    changed socket connection and added babel plugin

--- src/components/ReactRTC/ReactRTC.jsx
+++ src/components/ReactRTC/ReactRTC.jsx
@@ -1,29 +1,39 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import SocketConnectionMethods from './functions/socketConnection.js';
+// import SocketConnectionMethods from './functions/socketConnection.js';
 
 class ReactRTC extends React.Component {
   constructor() {
     super();
     this.state = {
-      peerConnection: null,
-      iceServerConfig: null,
-      SocketConnectionMethods: null,
+      // peerConnection: null,
+      // iceServerConfig: null,
+      // SocketConnectionMethods: null,
     };
   }
 
+  testSocket = new WebSocket('ws://localhost:3000/');
   componentDidMount() {
-    // const testSocket = new WebSocket('ws://localhost:3000/');
-    this.setState({
-      socketConnection: new WebSocket('ws://localhost:3000/'),
-      peerConnection: new RTCPeerConnection({
-        iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
-      }),
-    });
-  }
+    // this.setState({
+    //   socketConnection: new WebSocket('ws://localhost:3000/'),
+    //   peerConnection: new RTCPeerConnection({
+    //     iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
+    //   }),
+    // });
+    this.testSocket.onopen = () =>{
+      console.log('connected')
+    }
+    this.testSocket.onmessage = event =>{
+      const message = JSON.parse(event.data);
+      this.setState({dataFromServer: message})
+      console.log(message)
+    } 
+    this.testSocket.onclose = () => {
+      console.log('disconnected')
+    }
+   }
 
   render() {
-    this.state.socketConnection.onmessage = SocketConnectionMethods.onMessage();
+    // this.state.socketConnection.onmessage = SocketConnectionMethods.onMessage();
     return (
       <div>
         <h1>ReactRTC!!</h1>
